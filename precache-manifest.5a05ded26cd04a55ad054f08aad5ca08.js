510b39d195e88edb23e8ff96fca46f21e4835d66
GiraffeTheBozorgavar <130395135+GiraffeTheBozorgavar@users.noreply.github.com>
2023-04-10 20:52:41 +0300

    Add files via upload

---
+++ precache-manifest.5a05ded26cd04a55ad054f08aad5ca08.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9856637312802f872af3",
+    "url": "/css/app.90b2cc7a.css"
+  },
+  {
+    "revision": "6b2d17a00154bbdfe340",
+    "url": "/css/chunk-vendors.3f91cce5.css"
+  },
+  {
+    "revision": "dbc2aca73d99a254299277b55fa1f8df",
+    "url": "/img/instagram.dbc2aca7.svg"
+  },
+  {
+    "revision": "48ccd960a2ca1c58361f7ae3f8fd4559",
+    "url": "/img/telegram.48ccd960.svg"
+  },
+  {
+    "revision": "b32baf269e6237a1559019ca12675be1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9856637312802f872af3",
+    "url": "/js/app.1eb0f571.js"
+  },
+  {
+    "revision": "6b2d17a00154bbdfe340",
+    "url": "/js/chunk-vendors.15b5b635.js"
+  },
+  {
+    "revision": "5d8bfd20e4f0e20aa4379d8d74697ae2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
